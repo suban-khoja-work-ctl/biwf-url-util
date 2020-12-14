d2148d7740b777a5690cf801db22515d147e9fb4
suban-khoja-work-ctl <67577386+suban-khoja-work-ctl@users.noreply.github.com>
2020-12-14 18:46:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-const PARAM_CONTAINER=document.querySelector(".result-container > table"),formed_url={host:""},cretateParamsOnUI=a=>{for(param in a){let b=PARAM.content.cloneNode(!0);b.querySelector(".param-name").textContent=param,b.querySelector(".param-value").textContent=a[param],PARAM_CONTAINER.appendChild(b)}},processURL=a=>{if("Enter"===a.key&&a.currentTarget.value){console.log(a.currentTarget.value);try{const b=new URL(a.currentTarget.value);formed_url.host=b.host;const c=b.searchParams.keys(),d=b.searchParams.values();let e=c.next(),f=d.next();for(;!e.done&&!f.done;)formed_url[e.value]=f.value,e=c.next(),f=d.next();cretateParamsOnUI(formed_url)}catch(a){alert("Invalid URL")}}};
+const PARAM_CONTAINER = document.querySelector('.result-container > table');
+const formed_url = {
+    host :''
+}
+
+const cretateParamsOnUI = (_url) => {
+    for(param in _url){
+        let param_ele = PARAM.content.cloneNode(true);
+        param_ele.querySelector('.param-name').textContent = param;
+        param_ele.querySelector('.param-value').textContent = _url[param];
+        PARAM_CONTAINER.appendChild(param_ele);
+    }
+}
+
+const processURL = (e) => {
+    if(e.key === 'Enter' && e.currentTarget.value){
+        console.log(e.currentTarget.value);
+        try {
+            const _url = new URL(e.currentTarget.value);
+            formed_url.host = _url.host
+
+            const url_key_it = _url.searchParams.keys();
+            const url_val_it = _url.searchParams.values();
+
+            let param_key_obj = url_key_it.next();
+            let param_val_obj = url_val_it.next();
+            while(!param_key_obj.done && !param_val_obj.done){
+                formed_url[param_key_obj.value] = param_val_obj.value;
+                param_key_obj = url_key_it.next();
+                param_val_obj = url_val_it.next();
+            }
+
+            cretateParamsOnUI(formed_url);
+
+        }catch(err){
+            alert('Invalid URL');
+        }
+    }
+}
